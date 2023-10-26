5f267d89c54e34499dd65b2bddf1f792842ed2c4
cengizu <cengiz.ulku@gmail.com>
2023-10-26 17:25:22 +0300

    Add files via upload

---
+++ smiley/helpers.c
@@ -0,0 +1,19 @@
+#include "helpers.h"
+
+void colorize(int height, int width, RGBTRIPLE image[height][width])
+{
+    // Change all black pixels to a color of your choosing
+    for (int row = 0; row < height; row++)
+    {
+        for (int col = 0; col < width; col++)
+        {
+            if (image[row][col].rgbtRed == 0 && image[row][col].rgbtGreen == 0 && image[row][col].rgbtBlue == 0)
+            {
+                // set color to yellow
+                image[row][col].rgbtRed = 255;
+                image[row][col].rgbtGreen = 255;
+                image[row][col].rgbtBlue = 0;
+            }
+        }
+    }
+}
